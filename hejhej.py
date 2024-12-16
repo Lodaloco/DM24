87fd9a2b7eb1db22b11d4b99dac29cbc91b635ee
Lodaloco <maxstjernbernilsson@gmail.com>
2024-12-16 15:25:03 +0100

    change

--- hejhej.py
+++ hejhej.py
@@ -1,3 +1,7 @@
 frukter = ["äpple", "banan", "körsbär"]
 for frukt in frukter:
     print(frukt)
+text = "Hej, världen!"
+print(text.upper())    # HEJ, VÄRLDEN!
+print(text.lower())    # hej, världen!
+print(text.split(",")) # ['Hej', ' världen!']
